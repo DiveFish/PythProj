f58b6e3d3015160e3ce5c0d2a44d80fae8dd158a
DiveFish <DiveFish@users.noreply.github.com>
2016-02-05 07:50:18 +0100

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -3,20 +3,27 @@ from Tkinter import *
 master = Tk()
 
 
-def display_selection_level():
-    print "You've selected >>" + level.get() + "<< as your level. "
+def submitted_values():
+    print "You've selected the level: " + level.get()
+    print "You've selected the register: " + register.get()
+    if not userText.get():
+        print "No text was entered."
+        if not fileName.get():
+            print "No filename was entered."
+        else:
+            print "Warning: the file <" + fileName.get() + "> will be empty."
+    else:
+        print "You've requested the complexity level of the text: " + userText.get()
+        print "It will be saved in the file " + fileName.get()
 
+def save_to_file():
+    open(fileName.get() + ".txt", "w").write(userText.get())
 
-def display_selection_register():
-    print "You've selected: >>" + register.get() + "<< as your register. "
-
-
-def display_entered_sentence():
-    print "You've requested the complexity level of the sentence >>" + userSentence.get() + "<<. "
 
 
 emptyLine = Label(text=" ")
 emptyLine.pack()
+
 
 # drop down for choosing level
 chooseLevel = Label(text="Choose a level: ")
@@ -25,14 +32,8 @@ chooseLevel.pack()
 level = StringVar(master)
 level.set("easy") # initial value
 
-levelOptions = OptionMenu(master, level, "easy", "medium", "difficult")
+levelOptions = OptionMenu(master, level, "easy", "advanced", "difficult")
 levelOptions.pack()
-
-selectedLevel = level.get()
-
-# button to display level currently selected
-selLev = Button(master, text="Print selected level?", command=display_selection_level)
-selLev.pack()
 
 emptyLine = Label(text=" ")
 emptyLine.pack()
@@ -43,37 +44,44 @@ chooseRegister = Label(text="Choose a register: ")
 chooseRegister.pack()
 
 register = StringVar(master)
-register.set("newspaper") # initial value
+register.set("web chat") # initial value
 
-registerOptions = OptionMenu(master, register, "newspaper", "report", "essay")
+registerOptions = OptionMenu(master, register, "web chat", "newspaper", "report", "essay")
 registerOptions.pack()
-
-selectedRegister = register.get()
-
-
-# button to display register currently selected
-selReg = Button(master, text="Print selected register?", command=display_selection_register)
-selReg.pack()
 
 emptyLine = Label(text=" ")
 emptyLine.pack()
 
 
 # field to enter text for which user wants complexity level
-enterSentence = Label(text="Want a sentence of your own evaluated? Just enter it here: ")
+enterSentence = Label(text="Want a text of your own evaluated? Just enter it here: ")
 enterSentence.pack()
-userSentence=Entry(master)
-userSentence.pack()
+userText=Entry(master)
+userText.pack()
+enterFileName = Label(text="Please name the output file: ")
+enterFileName.pack()
+fileName=Entry(master)
+fileName.pack()
 
-enteredSentence = userSentence.get()
+
+emptyLine = Label(text=" ")
+emptyLine.pack()
+
 
 # button to display level currently selected
-unknownSent = Button(master, text="Print sentence entered by the user!", command=display_entered_sentence)
-unknownSent.pack()
+unknownText = Button(master, text="Print submitted values!", command=submitted_values)
+unknownText.pack()
+
+emptyLine = Label(text=" ")
+emptyLine.pack()
 
 
-f = Frame(master,height=40, width=300)  # only integers to use here
+# button to display level currently selected
+saveToFile = Button(master, text="Save output to a file!", command=save_to_file)
+saveToFile.pack()
 
+
+f = Frame(master,height=40, width=300)  # use only integers here
 f.pack_propagate(0)
 f.pack()
 
