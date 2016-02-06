3fbf43ad6281a3615e88e9c4898ec820f47ebdad
DiveFish <DiveFish@users.noreply.github.com>
2016-02-06 07:03:25 +0100

    Create TagSet.py

---
+++ TagSet.py
@@ -0,0 +1,14 @@
+# tags from the Penn Tree Bank
+# pronouns incl. wh-pronouns
+PennPronTags = ['PRP','PRP$','WP','WP$']
+# punctuation
+PennPunct = ['SYM','.']
+
+# tags from Brown corpus
+# pronouns incl. wh-pronouns
+BrownPronTags = ['DO+PPSS','DTS+BEZ','PN','PN$','PN+BEZ','PN+HVD','PN+HVZ','PN+MD','PP$$','PPL','PPLS','PPO',
+            'PPS','PPS+BEZ','PPS+HDV','PPS+HVZ','PPS+MD','PPSS','PPSS+BEM','PPSS+BER','PPSS+BEZ','PPSS+BEZ*',
+            'PPSS+HV','PPSS+HVD','PPSS+MD','PPSS+VB','VB+PPO','WDT+BER+PP','WDT+DO+PPS','WP$','WPO',
+            'WPS','WPS+BEZ','WPS+HVD','WPS+HVZ','WPS+MD']
+# punctuation
+BrownPunct = ['(',')',',','--','.',':']
