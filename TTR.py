202959657e8fd2ce9dabb42dc7b2af1ffcc4b451
kibbs <kmbrleyeo@gmail.com>
2016-02-20 17:32:19 +0100

    Update TTR.py

--- TTR.py
+++ TTR.py
@@ -1,10 +1,11 @@
 '''
 Created on 20 Feb 2016
+Updated on 21 Feb 2016
 
 @author: kibs
 '''
 
-from corpustags import news_words
+from corpustags import news_words, news_tagged
 
 # take a list of words
 def TTR(text):
@@ -19,5 +20,19 @@ def TTR(text):
             type = type + 1.0
     return type/tokens 
 
-print news_words
+def TTR_tagged(tagged_text):
+    tokens = len(tagged_text)
+    type = 0.0
+    word_list = list()
+    for (word, tag) in tagged_text:
+        if word_list.__contains__(word):
+            continue
+        else:
+            word_list.append(word)
+            type = type + 1.0
+    return type/tokens
+
+#print news_words
 print TTR(news_words)
+
+print TTR_tagged(news_tagged)
