35bf0bc8b92ef129e91a4e094d451e75ad23cd1e
DiveFish <DiveFish@users.noreply.github.com>
2016-02-20 16:56:54 +0100

    Update pronoun_count.py

--- pronoun_count.py
+++ pronoun_count.py
@@ -1,7 +1,8 @@
-'''
+"""
 Created on 20 Feb 2016
 @author: pat
-'''
+"""
+
 from corpustags import news_tagged_sents, editorial_tagged_sents, review_tagged_sents
 from nps_tags import nps_chat_tagged
 from tag_set import penn_pron_tags, brown_pron_tags
@@ -29,3 +30,6 @@ print 'chat pronouns: ' + str(count_pronouns_corpus(nps_chat_tagged))
 print 'news pronouns: ' + str(count_pronouns_corpus(news_tagged_sents))
 print 'editorial pronouns: ' + str(count_pronouns_corpus(editorial_tagged_sents))
 print 'review pronouns: ' + str(count_pronouns_corpus(review_tagged_sents))
+
+sentence = [('Who','WPO'), ('do','V'), ('you','PPSS'), ('love','V'), ('?','.')]
+print 'review pronouns (sentence): ' + str(count_pronouns_sent(sentence))
