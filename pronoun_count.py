481bd9423dd7d1232bb4cf76e42138f34ae52aef
DiveFish <DiveFish@users.noreply.github.com>
2016-02-20 10:57:15 +0100

    Update pronoun_count.py

--- pronoun_count.py
+++ pronoun_count.py
@@ -6,12 +6,13 @@ from corpustags import news_tagged_sents, editorial_tagged_sents, review_tagged_
 from nps_tags import nps_chat_tagged
 from tag_set import penn_pron_tags, brown_pron_tags
 
+
 def count_pronouns(tagged_corpus):
     num_of_pronouns = 0
     for sent in tagged_corpus:
         for (word, tag) in sent:
             if (tag in brown_pron_tags) or (tag in penn_pron_tags):
-                num_of_pronouns = num_of_pronouns + 1
+                num_of_pronouns += 1
                 print word + ' ' + tag
     return num_of_pronouns
 
