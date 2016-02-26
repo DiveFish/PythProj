8cc93fb53c6f0cc568f1891a9b3775b18a9b9629
kibbs <kmbrleyeo@gmail.com>
2016-02-26 17:47:02 +0100

    Update and rename TTR.py to ttr.py
    now has 4 ttr functions taking different formats

--- ttr.py
+++ ttr.py
@@ -4,16 +4,20 @@ Updated on 24 Feb 2016
 
 @author: kibs
 '''
-import nltk
-import re
-from nltk.corpus import brown 
 
+'''
+import nltk
+from nltk.corpus import brown 
+'''
+
+import re
 def is_word(string):
-    find_letter = re.search('[a-z]', string)
-    return bool(find_letter)
+    find_letter = re.search('[a-z0-9]', string)
+    letter_in_string = bool(find_letter)
+    return letter_in_string
 
 # take a list of words
-def TTR(text):
+def TTR_words(text):
     tokens = 0.0
     type = 0.0
     word_list = list()
@@ -30,7 +34,8 @@ def TTR(text):
             type += 1
     return type/tokens 
 
-def TTR_tagged(tagged_text):
+# takes a list of (word, tag)
+def TTR_tagged_words(tagged_text):
     tokens = 0.0
     type = 0.0
     word_list = list()
@@ -47,6 +52,7 @@ def TTR_tagged(tagged_text):
             type += 1
     return type/tokens
 
+#takes a list of lists (sentences) of (word, tag)
 def TTR_tagged_sents(tagged_sents):
     tokens = 0.0
     type = 0.0
@@ -65,6 +71,26 @@ def TTR_tagged_sents(tagged_sents):
                 type += 1
     return type/tokens
 
+# takes list of lists(lists) of words
+def TTR_sents(sents):
+    tokens = 0.0
+    type = 0.0
+    word_list = list()
+    for sent in sents:
+        for word in sent:
+            word = word.lower()
+            if is_word(word):
+                tokens += 1
+            else:
+                continue
+            if word_list.__contains__(word):
+                continue
+            else:
+                word_list.append(word)
+                type += 1
+    return type/tokens
+
+'''
 print 'TTR of list containing symbols: ' + str(TTR(['a','b','c','d','e','e','.','.']))
 
 news_words = brown.words(fileids="ca16")
@@ -73,3 +99,4 @@ news_tagged = brown.tagged_words(fileids="ca16")
 print 'TTR of list of (word, tag):     ' + str(TTR_tagged(news_tagged))
 news_tagged_sents = brown.tagged_sents(fileids='ca16')
 print 'TTR of list of lists:           ' + str(TTR_tagged_sents(news_tagged_sents))
+'''
