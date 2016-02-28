ad7e04af746cb597c615522f5c2bab9e72b8e01c
ankita83 <ankitaoswal@gmail.com>
2016-02-28 09:00:47 +0100

    Update ReadFile.py

--- ReadFile.py
+++ ReadFile.py
@@ -9,31 +9,31 @@ import os
 import sys
 
 def read_file():
+
     #newpath is the path where the folder for the output files is created
-    newpath = 'C:\Users\Ankita\Desktop\WS 2015_16\Python\Project\Proj\OutputFiles'
+    newpath = 'C:\Users\Ankita\Desktop\WS 2015_16\Python\Project\Proj\InputFiles\ParsedFiles'
     if not os.path.exists(newpath):
         os.makedirs(newpath)
-    
-    #"InputFiles" is the folder containing all input files
-    list_of_files = os.listdir("InputFiles")
-    for x in list_of_files:
-        f = open(x, "r") #I get an error here. It cannot recognise the filename. Have tried giving the complete path but same error
-        y = os.path.basename(f.name)
-        
-        #Modify path accordingly to path of output file folder
-        filename = open("C:\Users\Ankita\Desktop\WS 2015_16\Python\Project\Proj\OutputFiles" + y + ".txt",'w')
-        sys.stdout = filename
-        line = f.readline()
-        while line:
-            text = nltk.word_tokenize(line)
-            print text
-            tags = nltk.pos_tag(text)
-            print tags
-            counts = Counter(tag for word,tag in tags)
-            print counts
-            print '\n'
+
+    os.chdir(r"C:\Users\Ankita\Desktop\WS 2015_16\Python\Project\Proj\InputFiles")
+
+    for file in os.listdir("C:\Users\Ankita\Desktop\WS 2015_16\Python\Project\Proj\InputFiles"):
+        if file.endswith(".txt"):
+            f = open(file, "r")
+            y = "parsed_" + os.path.basename(f.name)
+
+            filename = open(os.path.join(newpath,y) ,'w')
+            sys.stdout = filename
             line = f.readline()
-        f.close()
+            while line:
+                text = nltk.word_tokenize(line)
+                print text
+                tags = nltk.pos_tag(text)
+                print tags
+                counts = Counter(tag for word,tag in tags)
+                print counts
+                print '\n'
+                line = f.readline()
+            f.close()
 
 read_file()
-
