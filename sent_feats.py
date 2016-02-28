4822337336ac52e20c4004f4acb43b2be3c4fc47
DiveFish <DiveFish@users.noreply.github.com>
2016-02-28 19:59:15 +0100

    added average word length

--- sent_feats.py
+++ sent_feats.py
@@ -1,10 +1,11 @@
 """
 Created on 21 Feb 2016
-@author: pat & anki
+@author: Pat
 """
 
 from corpustags import news_tagged_sents, editorial_tagged_sents, review_tagged_sents
 from nps_tags import nps_chat_tagged
+from tag_set import penn_punct, brown_punct
 
 
 def sentence_length_corpus(tagged_corpus):
@@ -33,7 +34,32 @@ def num_of_sent(tagged_corpus):
     return sent_count
 
 
-def average_sent_length(tagged_corpus):
+def single_sent_word_length(sentence):
+    total_num_of_letters = 0
+    word_count = 0
+    for (word, tag) in sentence:
+        if not (penn_punct.__contains__(tag) or brown_punct.__contains__(tag)):
+            word_count += 1.0
+            for letter in word:
+                total_num_of_letters+=1.0
+    average_word_length = total_num_of_letters/word_count
+    return average_word_length
+
+
+def word_length_average(tagged_corpus):
+    total_num_of_letters = 0
+    word_count = 0
+    for sent in tagged_corpus:
+        for (word, tag) in sent:
+            if not (penn_punct.__contains__(tag) or brown_punct.__contains__(tag)):
+                word_count += 1.0
+                for letter in word:
+                    total_num_of_letters+=1.0
+    average_word_length = total_num_of_letters/word_count
+    return average_word_length
+
+
+def sent_length_average(tagged_corpus):
     total_length = 0
     sent_count = 0
     for sent in tagged_corpus:
@@ -42,8 +68,8 @@ def average_sent_length(tagged_corpus):
         for (word, tag) in sent:
             sent_length += 1.0
         total_length = total_length+sent_length
-    average_length = total_length/sent_count
-    return average_length
+    average_sentence_length = total_length/sent_count
+    return average_sentence_length
 
 
 # Test sentence length for text from Brown corpus
@@ -68,7 +94,16 @@ print "Length of a single sentence: " + str(single_sent_length(sentence))
 
 
 # Test average sentence length for all corpora
-print "Average sentence length of chat corpus: " + str(average_sent_length(nps_chat_tagged))
-print "Average sentence length of news corpus: " + str(average_sent_length(news_tagged_sents))
-print "Average sentence length of editorial corpus: " + str(average_sent_length(editorial_tagged_sents))
-print "Average sentence length of review corpus: " + str(average_sent_length(review_tagged_sents))
+print "Average sentence length of chat corpus: " + str(sent_length_average(nps_chat_tagged))
+print "Average sentence length of news corpus: " + str(sent_length_average(news_tagged_sents))
+print "Average sentence length of editorial corpus: " + str(sent_length_average(editorial_tagged_sents))
+print "Average sentence length of review corpus: " + str(sent_length_average(review_tagged_sents))
+
+print "\n"
+print "Average word length of chat corpus: " + str(word_length_average(nps_chat_tagged))
+print "Average word length of news corpus: " + str(word_length_average(news_tagged_sents))
+print "Average word length of editorial corpus: " + str(word_length_average(editorial_tagged_sents))
+print "Average word length of review corpus: " + str(word_length_average(review_tagged_sents))
+
+sentence = [("Who","WPO"), ("did","DO"), ("you","PPSS"), ("love","VB"), ("?",".")]
+print "Average length of words in single sentence: " + str(single_sent_word_length(sentence))
