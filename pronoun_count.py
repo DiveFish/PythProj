c25695665a6076884c5933229c621249223106e1
DiveFish <DiveFish@users.noreply.github.com>
2016-03-05 13:06:49 +0100

    Update pronoun_count.py

--- pronoun_count.py
+++ pronoun_count.py
@@ -18,14 +18,6 @@ def count_pronouns_corpus(tagged_corpus):
         for (word, tag) in sent:
             if (brown_pron_tags.__contains__(tag)) or (penn_pron_tags.__contains__(tag)):
                 num_of_pronouns += 1.0
-                # print word + ' ' + tag
-            '''
-            #to check that no word which should be counted has been missed
-            if word == "you":
-                print '   ' + word
-            '''
-    # print num_of_pronouns
-    # print num_of_sents
     pron_per_sent = num_of_pronouns/num_of_sents
     return pron_per_sent
 
@@ -35,20 +27,4 @@ def count_pronouns_sent(tagged_sent):
     for (word, tag) in tagged_sent:
         if (brown_pron_tags.__contains__(tag)) or (penn_pron_tags.__contains__(tag)):
             num_of_pronouns += 1
-            # print word + ' ' + tag
-        '''
-        #to check that no word which should be counted has been missed
-        if word == "you":
-            print '   ' + word
-        '''
     return num_of_pronouns
-
-
-print 'PRONOUN COUNTS FOR ACCESSED CORPORA'
-print 'average of chat pronouns: ' + str(count_pronouns_corpus(nps_chat_tagged))
-print 'average of news pronouns: ' + str(count_pronouns_corpus(news_tagged_sents))
-print 'average of editorial pronouns: ' + str(count_pronouns_corpus(editorial_tagged_sents))
-print 'average of review pronouns: ' + str(count_pronouns_corpus(review_tagged_sents))
-
-sentence = [('Who','WPO'), ('do','DO'), ('you','PPSS'), ('love','VB'), ('?','.')]
-print 'Pronouns in a random sentence: ' + str(count_pronouns_sent(sentence))
