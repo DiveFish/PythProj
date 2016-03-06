6083b14a86e595060bb6805048a77ddf7d54afae
DiveFish <DiveFish@users.noreply.github.com>
2016-03-06 07:38:03 +0100

    !!! please use new pronoun count (per words)

--- pronoun_count.py
+++ pronoun_count.py
@@ -1,11 +1,9 @@
 """
 Created on 20 Feb 2016
-Updated on 05 March 2016
-@author: pat
+Updated on 06 March 2016
+@author: Pat
 """
 
-from corpustags import news_tagged_sents, editorial_tagged_sents, review_tagged_sents
-from nps_tags import nps_chat_tagged
 from tag_set import penn_pron_tags, brown_pron_tags
 
 
@@ -22,7 +20,19 @@ def count_pronouns_corpus(tagged_corpus):
     return pron_per_sent
 
 
-# calculate number of pronouns in a single sentence
+# calculate ratio of pronouns per total number of words in a corpus
+def count_pronouns_per_words(tagged_corpus):
+    num_of_pronouns = 0
+    num_of_words = 0
+    for sent in tagged_corpus:
+        for (word, tag) in sent:
+            num_of_words += 1.0
+            if (brown_pron_tags.__contains__(tag)) or (penn_pron_tags.__contains__(tag)):
+                num_of_pronouns += 1.0
+    pron_per_word_total = num_of_words/num_of_pronouns
+    return pron_per_word_total
+
+
 def count_pronouns_sent(tagged_sent):
     num_of_pronouns = 0
     for (word, tag) in tagged_sent:
