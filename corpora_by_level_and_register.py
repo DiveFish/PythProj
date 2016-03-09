f7729476de05850a696bc2131ee773dbeece63ba
DiveFish <DiveFish@users.noreply.github.com>
2016-03-09 10:11:44 +0100

    Update corpora_by_level_and_register.py

--- corpora_by_level_and_register.py
+++ corpora_by_level_and_register.py
@@ -13,22 +13,22 @@ for sentence in brown.tagged_sents(fileids="ca43"):
     news_easy.append(sentence)
 for sentence in brown.tagged_sents(fileids="ca32"):
     news_easy.append(sentence)
-for sentence in brown.tagged_sents(fileids="ca30"):
+for sentence in brown.tagged_sents(fileids="ca07"):
     news_easy.append(sentence)
-for sentence in brown.tagged_sents(fileids="ca39"):
+for sentence in brown.tagged_sents(fileids="ca30"):
     news_easy.append(sentence)
 
 
 news_advanced = list()
-for sentence in brown.tagged_sents(fileids="ca25"):
+for sentence in brown.tagged_sents(fileids="ca42"):
     news_advanced.append(sentence)
 for sentence in brown.tagged_sents(fileids="ca18"):
     news_advanced.append(sentence)
-for sentence in brown.tagged_sents(fileids="ca22"):
+for sentence in brown.tagged_sents(fileids="ca24"):
     news_advanced.append(sentence)
-for sentence in brown.tagged_sents(fileids="ca12"):
+for sentence in brown.tagged_sents(fileids="ca08"):
     news_advanced.append(sentence)
-for sentence in brown.tagged_sents(fileids="ca13"):
+for sentence in brown.tagged_sents(fileids="ca11"):
     news_advanced.append(sentence)
 
 
@@ -57,17 +57,17 @@ for sentence in brown.tagged_sents(fileids="cb23"):
 for sentence in brown.tagged_sents(fileids="cb17"):
     editorial_easy.append(sentence)
 
-
+# done
 editorial_advanced = list()
 for sentence in brown.tagged_sents(fileids="cb24"):
     editorial_advanced.append(sentence)
 for sentence in brown.tagged_sents(fileids="cb01"):
     editorial_advanced.append(sentence)
-for sentence in brown.tagged_sents(fileids="cb12"):
+for sentence in brown.tagged_sents(fileids="cb05"):
     editorial_advanced.append(sentence)
-for sentence in brown.tagged_sents(fileids="cb15"):
+for sentence in brown.tagged_sents(fileids="cb20"):
     editorial_advanced.append(sentence)
-for sentence in brown.tagged_sents(fileids="cb06"):
+for sentence in brown.tagged_sents(fileids="cb27"):
     editorial_advanced.append(sentence)
 
 
@@ -80,7 +80,7 @@ for sentence in brown.tagged_sents(fileids="cb22"):
     editorial_difficult.append(sentence)
 for sentence in brown.tagged_sents(fileids="cb07"):
     editorial_difficult.append(sentence)
-for sentence in brown.tagged_sents(fileids="cb02"):
+for sentence in brown.tagged_sents(fileids="cb03"):
     editorial_difficult.append(sentence)
 
 
@@ -98,11 +98,11 @@ for sentence in brown.tagged_sents(fileids="cc10"):
 
 
 reviews_advanced = list()
-for sentence in brown.tagged_sents(fileids="cc16"):
+for sentence in brown.tagged_sents(fileids="cc05"):
     reviews_advanced.append(sentence)
 for sentence in brown.tagged_sents(fileids="cc04"):
     reviews_advanced.append(sentence)
-for sentence in brown.tagged_sents(fileids="cc11"):
+for sentence in brown.tagged_sents(fileids="cc13"):
     reviews_advanced.append(sentence)
 for sentence in brown.tagged_sents(fileids="cc02"):
     reviews_advanced.append(sentence)
@@ -130,18 +130,18 @@ for sentence in brown.tagged_sents(fileids="ch01"):
     government_easy.append(sentence)
 for sentence in brown.tagged_sents(fileids="ch07"):
     government_easy.append(sentence)
-for sentence in brown.tagged_sents(fileids="ch24"):
-    government_easy.append(sentence)
 for sentence in brown.tagged_sents(fileids="ch04"):
+    government_easy.append(sentence)
+for sentence in brown.tagged_sents(fileids="ch28"):
     government_easy.append(sentence)
 
 
 government_advanced = list()
 for sentence in brown.tagged_sents(fileids="ch14"):
     government_advanced.append(sentence)
-for sentence in brown.tagged_sents(fileids="ch13"):
+for sentence in brown.tagged_sents(fileids="ch06"):
     government_advanced.append(sentence)
-for sentence in brown.tagged_sents(fileids="ch10"):
+for sentence in brown.tagged_sents(fileids="ch30"):
     government_advanced.append(sentence)
 for sentence in brown.tagged_sents(fileids="ch20"):
     government_advanced.append(sentence)
@@ -163,38 +163,38 @@ for sentence in brown.tagged_sents(fileids="ch23"):
 
 
 chat_easy = list()
+for post in nps_chat.tagged_posts(fileids="11-08-teens_706posts.xml"):
+    chat_easy.append(post)
+for post in nps_chat.tagged_posts(fileids="11-08-40s_706posts.xml"):
+    chat_easy.append(post)
+for post in nps_chat.tagged_posts(fileids="11-06-adults_706posts.xml"):
+    chat_easy.append(post)
 for post in nps_chat.tagged_posts(fileids="10-19-20s_706posts.xml"):
     chat_easy.append(post)
 for post in nps_chat.tagged_posts(fileids="11-08-20s_705posts.xml"):
     chat_easy.append(post)
-for post in nps_chat.tagged_posts(fileids="10-19-30s_705posts.xml"):
-    chat_easy.append(post)
-for post in nps_chat.tagged_posts(fileids="11-09-adults_706posts.xml"):
-    chat_easy.append(post)
-for post in nps_chat.tagged_posts(fileids="11-08-teens_706posts.xml"):
-    chat_easy.append(post)
 
 
 chat_advanced = list()
-for post in nps_chat.tagged_posts(fileids="11-08-adults_705posts.xml"):
-    chat_advanced.append(post)
-for post in nps_chat.tagged_posts(fileids="11-09-20s_706posts.xml"):
-    chat_advanced.append(post)
-for post in nps_chat.tagged_posts(fileids="10-19-adults_706posts.xml"):
-    chat_advanced.append(post)
 for post in nps_chat.tagged_posts(fileids="11-09-teens_706posts.xml"):
     chat_advanced.append(post)
-for post in nps_chat.tagged_posts(fileids="10-26-teens_706posts.xml"):
+for post in nps_chat.tagged_posts(fileids="11-09-adults_706posts.xml"):
+    chat_advanced.append(post)
+for post in nps_chat.tagged_posts(fileids="10-24-40s_706posts.xml"):
+    chat_advanced.append(post)
+for post in nps_chat.tagged_posts(fileids="10-19-40s_686posts.xml"):
+    chat_advanced.append(post)
+for post in nps_chat.tagged_posts(fileids="11-08-adults_705posts.xml"):
     chat_advanced.append(post)
 
 chat_difficult = list()
-for post in nps_chat.tagged_posts(fileids="11-06-adults_706posts.xml"):
+for post in nps_chat.tagged_posts(fileids="10-19-30s_705posts.xml"):
     chat_difficult.append(post)
-for post in nps_chat.tagged_posts(fileids="11-08-40s_706posts.xml"):
-    chat_difficult.append(post)
-for post in nps_chat.tagged_posts(fileids="10-19-40s_686posts.xml"):
+for post in nps_chat.tagged_posts(fileids="11-09-20s_706posts.xml"):
     chat_difficult.append(post)
 for post in nps_chat.tagged_posts(fileids="11-09-40s_706posts.xml"):
     chat_difficult.append(post)
-for post in nps_chat.tagged_posts(fileids="10-24-40s_706posts.xml"):
+for post in nps_chat.tagged_posts(fileids="10-19-adults_706posts.xml"):
+    chat_difficult.append(post)
+for post in nps_chat.tagged_posts(fileids="10-26-teens_706posts.xml"):
     chat_difficult.append(post)
